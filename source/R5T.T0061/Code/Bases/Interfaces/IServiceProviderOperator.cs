46fb0a940c5b64927d437aaf6a85e5875861ebc3
David Coats <david@getsafetycone.com>
2021-10-28 01:13:18 -0700

    Add extension method base types.

---
+++ source/R5T.T0061/Code/Bases/Interfaces/IServiceProviderOperator.cs
@@ -0,0 +1,15 @@
+using System;
+
+using R5T.T0039.T002;
+
+
+namespace R5T.T0061
+{
+    /// <summary>
+    /// Empty interface as base for extension methods.
+    /// </summary>
+    [ExtensionMethodBaseMarker]
+    public interface IServiceProviderOperator
+    {
+    }
+}
